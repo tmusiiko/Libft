8c5d08d87ffeb049df8f365788d9e7da02766a34
taras <taras.musiiko@gmail.com>
2021-08-23 14:28:57 +0200

    Igor

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,33 @@
 /*   By: tmusiiko <tmusiiko@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 00:23:44 by taras             #+#    #+#             */
-/*   Updated: 2021/08/17 09:45:53 by tmusiiko         ###   ########.fr       */
+/*   Updated: 2021/08/23 14:25:10 by tmusiiko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char	*big, const char *little, size_t len)
-
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	j;
 	size_t	i;
+	size_t	j;
 
-	j = 0;
+	if (little[0] == '\0')
+		return ((char *)big);
 	i = 0;
-	if (little[i] == '\0')
-		return ((char *)&big[j]);
-	while (big[j] != '\0' && j < len)
+	while (big[i] && i < len)
 	{
-		while (j < len && little[i] == big[j] && little[i] != '\0')
-		{	
-			i++;
-			j++;
+		j = 0;
+		if (big[i] == little[j])
+		{
+			while (i + j < len && big[i + j] == little[j])
+			{
+				j++;
+				if (!little[j])
+					return ((char *)&big[i]);
+			}
 		}
-		if (little[i] == '\0')
-			return ((char *)&big[j - i]);
-		else if (i != 0)
-			return (0);
-		j++;
+		i++;
 	}
 	return (0);
 }
